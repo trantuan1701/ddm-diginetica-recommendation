e3c273dfb3d94784c4efff76e88f7ae94956edf8
Long <lytlong.pers@gmail.com>
2026-05-07 08:54:16 +0700

    chore: update dvc & environment setup

---
+++ scripts/download_data.sh
@@ -0,0 +1,16 @@
+# Note: Please install necessary packages before running this script
+
+# python packages
+
+# direnv: this is what we will use to load env variables. 
+# For this one, create a file called `.envrc` and paste the content:
+#
+# dotenv
+#
+
+dvc remote add gdrive gdrive://$GOOGLE_DRIVE_ID/dvc
+dvc remote modify myremote gdrive_acknowledge_abuse true
+dvc remote modify --local gdrive gdrive_client_id $GOOGLE_DRIVE_CLIENT_ID
+dvc remote modify --local gdrive gdrive_client_secret $GOOGLE_DRIVE_CLIENT_SECRET
+
+dvc pull
